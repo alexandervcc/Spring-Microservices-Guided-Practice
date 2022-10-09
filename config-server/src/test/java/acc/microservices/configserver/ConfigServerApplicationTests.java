ada8374e8af70603730575dec1ab967259d45342
Alexander Cuenca <alex.charco.25@gmail.com>
2022-10-09 14:47:57 -0500

    Init: config server

---
+++ config-server/src/test/java/acc/microservices/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package acc.microservices.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
