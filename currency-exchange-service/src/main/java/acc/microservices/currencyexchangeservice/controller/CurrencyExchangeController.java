a1eac3732130483b9427cfddd084e4080bb4eacb
Alexander Cuenca <alex.charco.25@gmail.com>
2022-10-10 18:07:35 -0500

    Retrieving local Port

--- currency-exchange-service/src/main/java/acc/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/acc/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,14 +2,23 @@ package acc.microservices.currencyexchangeservice.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 public class CurrencyExchangeController {
+
+    private Environment env;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(0));
+        CurrencyExchange ce = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(75));
+        ce.setEnvironment(env.getProperty("local.server.port"));
+        return ce;
     }
 }
