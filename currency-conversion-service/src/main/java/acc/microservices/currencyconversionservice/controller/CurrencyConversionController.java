fee9af0219bd3e4c901f4e40779212b451ee09da
Alexander Cuenca <alex.charco.25@gmail.com>
2022-10-10 18:56:40 -0500

    Conversion -> Exchange consumes

--- currency-conversion-service/src/main/java/acc/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/acc/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,11 +1,13 @@
 package acc.microservices.currencyconversionservice.controller;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
-import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import lombok.AllArgsConstructor;
 
@@ -13,22 +15,34 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class CurrencyConversionController {
 
-    private Environment environment;
-
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
             @PathVariable String from,
             @PathVariable String to,
             @PathVariable BigDecimal quantity) {
 
+        // Path values
+        HashMap<String, String> uriVariables = new HashMap<String, String>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        // Http Client
+        ResponseEntity<CurrencyConversion> response = new RestTemplate()
+                .getForEntity(
+                        "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                        CurrencyConversion.class,
+                        uriVariables);
+
+        CurrencyConversion cc = response.getBody();
+
         return CurrencyConversion.builder()
                 .id(1001L)
                 .from(from)
                 .to(to)
                 .quantity(quantity)
-                .conversionMultiple(BigDecimal.ONE)
-                .totalCalculatedAmount(BigDecimal.ONE)
-                .environment(environment.getProperty("local.server.port"))
+                .conversionMultiple(cc.getConversionMultiple())
+                .totalCalculatedAmount(quantity.multiply(cc.getConversionMultiple()))
+                .environment(cc.getEnvironment())
                 .build();
     }
 }
